eea31b1f538b8d4c8b84d5522410d993944fffe8
Enrique <niofis@gmail.com>
2014-09-16 18:22:20 -0700

    Camera

---
+++ camera.js
@@ -0,0 +1,18 @@
+
+var Camera = (function () {
+    var camera = function () {
+        var self = this;
+
+        self.left_top = {
+        };
+        self.right_top = {};
+        self.left_bottom = {};
+    }
+
+
+    return camera;
+}
+
+
+
+module.exports = Camera;
