6e8053b0725e562499a2ad02bbc0025e13f43daf
Enrique <niofis@gmail.com>
2015-11-07 18:09:15 -0700

    Implemented pixi.js

--- light/light.js
+++ light/light.js
@@ -7,7 +7,20 @@ class Light {
 
   render(callback) {
     callback = callback || ()=>{};
-    callback();
+    var buffer = [];
+
+    for(var y = 0; y < 480; ++y) {
+      buffer[y] = [];
+      for(var x = 0; x < 640; ++x) {
+          var ray = {
+            origin: {x: 0, y: 0, z: 0},
+            direction: {x:0, y:0, z:0}
+          };
+          buffer[y][x] = 0xFF0000;
+        }
+    }
+
+    callback(buffer);
   }
 }
 
